0b47c888fd992b5cc1cb0a1094bdf9389ae391a6
Meenakshy <meenakshyprasanth7@gmail.com>
2026-06-27 10:43:47 +0530

    update allowed hosts for pythonanywhere

--- evora/evora/settings.py
+++ evora/evora/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o*tk!r3)1l&ti)*sh*cz&_jraadb+hb43w90plfr%)781q+4==
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['meenakshy.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
